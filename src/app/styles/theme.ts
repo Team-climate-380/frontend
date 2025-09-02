d4afb78c6b95e659d61c15e50ef0b1c6a637de7e
Polina Savateeva <polina.savateeva@geogracom.com>
2025-09-02 23:31:31 +0300

    feat: добавляет тему mantine

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme, MantineColorsTuple } from '@mantine/core'
+
+const usedColors: MantineColorsTuple = [
+  '#ffffff',
+  '#f6f8fA',
+  '#cdcdcd',
+  '#b2b2b2',
+  '#9a9a9a',
+  '#8b8b8b',
+  '#848484',
+  '#717171',
+  '#06121e',
+  '#000000'
+]
+
+export const theme = createTheme({
+  colors: { usedColors },
+  primaryColor: 'usedColors'
+})
