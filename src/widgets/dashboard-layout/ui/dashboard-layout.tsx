fd2104efe5dcf3f9fe1e75ff61219bb4d95b6c28
Elvira <l.angelich@yandex.ru>
2025-09-04 00:49:29 +0300

    feat: dashboard-layout

--- src/widgets/dashboard-layout/ui/dashboard-layout.tsx
+++ src/widgets/dashboard-layout/ui/dashboard-layout.tsx
@@ -1,11 +1,23 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router'
 import { Loader } from '@shared/ui/loader'
+import { ScrollArea } from '@mantine/core'
+import classes from './dashboard-layout.module.css'
+import Logo from './images/logo.svg?react'
+import SidebarImage from './images/sidebar-image.svg?react'
 
-export const DashboardLayout: React.FC = () => {
+export const DashboardLayout: React.FC = ({ contentSidebar }: ReactNode) => {
   return (
-    <div>
+    <div className={classes.container}>
       <Suspense fallback={<Loader />}>
+        <nav className={classes.navbar}>
+          <Logo alt="логотип" className={classes.logo} />
+          <ScrollArea type={'auto'} className={classes.scrollArea}>
+            {contentSidebar}
+          </ScrollArea>
+          <SidebarImage alt="девушка сидит на столе" className={classes.sidebar} />
+        </nav>
+
         <Outlet />
       </Suspense>
     </div>
