d14bd989d6f44e32474155fbcad8528aa561aff4
Elvira <l.angelich@yandex.ru>
2025-09-10 00:22:54 +0300

    add appshell, delete svgr(), change size container

--- src/widgets/dashboard-layout/ui/dashboard-layout.tsx
+++ src/widgets/dashboard-layout/ui/dashboard-layout.tsx
@@ -1,10 +1,8 @@
 import { Suspense, ReactNode } from 'react'
 import { Outlet } from 'react-router'
 import { Loader } from '@shared/ui/loader'
-import { ScrollArea } from '@mantine/core'
+import { AppShell } from '@mantine/core'
 import classes from './dashboard-layout.module.css'
-import Logo from './images/logo.svg?react'
-import SidebarImage from './images/sidebar-image.svg?react'
 
 interface DashboardLayoutProps {
   contentSidebar: ReactNode
@@ -13,13 +11,17 @@ interface DashboardLayoutProps {
 export const DashboardLayout: React.FC<DashboardLayoutProps> = ({ contentSidebar }) => {
   return (
     <div className={classes.container}>
-      <Logo role="img" aria-label="Логотип" className={classes.logo} />
-      <nav className={classes.navbar}>
-        <ScrollArea type={'auto'} className={classes.scrollArea}>
-          {contentSidebar}
-        </ScrollArea>
-      </nav>
-      <SidebarImage aria-hidden="true" className={classes.sidebar} />
+      <img src="./images/logo.svg" alt="Логотип" className={classes.logo} />
+      <AppShell
+        navbar={{
+          width: 193,
+          breakpoint: 'sm'
+        }}
+        className={classes.navbar}
+      >
+        {contentSidebar}
+      </AppShell>
+      <img src="./images/sidebarImage.svg" aria-hidden="true" className={classes.sidebar} />
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
