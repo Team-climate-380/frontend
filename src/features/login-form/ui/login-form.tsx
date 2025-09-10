cdccce1a561a55897630eade993b20912d15e54e
Даниил <khromov865@mail.ru>
2025-09-10 10:42:01 +0300

    fix: separated business logic, added email validation

--- src/features/login-form/ui/login-form.tsx
+++ src/features/login-form/ui/login-form.tsx
@@ -1,62 +1,42 @@
 import { Button } from '@/shared/ui/button'
 import { Flex, Group } from '@mantine/core'
-import { useForm } from '@mantine/form'
 import { FC } from 'react'
 import { Input } from '@/shared/ui/input'
 import { Checkbox } from '@/shared/ui/checkbox'
 import { PasswordInput } from '@/shared/ui/password-input'
+import { useFormData } from '../model/use-login-form'
+
+export interface InitialFormData {
+  email: string
+  password: string
+  rememberMy: boolean
+}
 
 interface LoginFormProps {
   onRestorePassword: () => void
-  initialValues?: {
-    email: string
-    password: string
-    rememberMy: boolean
-  }
+  initialValues?: InitialFormData
 }
 
 export const LoginForm: FC<LoginFormProps> = ({ onRestorePassword, initialValues }) => {
-  const form = useForm({
-    mode: 'controlled',
-    initialValues: initialValues ?? {
-      email: '',
-      password: '',
-      rememberMy: false
-    },
-    validate: {
-      password: value => {
-        if (!value) {
-          return 'Пароль не может быть пустым'
-        }
-        if (value.length < 8) {
-          return 'Пароль должен быть не менее 8 символов'
-        }
-        if (/^\d+$/.test(value)) {
-          return 'Пароль не должен состоять только из цифр'
-        }
-        return null
-      }
-    },
-    validateInputOnChange: true
-  })
+  const formData = useFormData(initialValues)
   return (
-    <form onSubmit={form.onSubmit(data => console.log(data))}>
+    <form onSubmit={formData.onSubmit(data => console.log(data))}>
       <Flex direction="column" gap="36px" w="258px">
         <Flex direction="column" gap="26px">
           <Flex direction="column" gap="20px">
-            <Input label="Почта" variant="secondary" key={form.key('email')} {...form.getInputProps('email')} />
+            <Input label="Почта" variant="secondary" key={formData.key('email')} {...formData.getInputProps('email')} />
             <PasswordInput
               label="Пароль"
               variant="secondary"
-              key={form.key('password')}
-              {...form.getInputProps('password')}
+              key={formData.key('password')}
+              {...formData.getInputProps('password')}
             />
           </Flex>
-          <Checkbox label="Запомнить" {...form.getInputProps('rememberMy', { type: 'checkbox' })} />
+          <Checkbox label="Запомнить" {...formData.getInputProps('rememberMy', { type: 'checkbox' })} />
         </Flex>
 
         <Group justify="space-between">
-          <Button type="submit" disabled={!form.isValid} size="md">
+          <Button type="submit" disabled={!formData.isValid} size="md">
             Войти
           </Button>
           <Button onClick={onRestorePassword} variant="ghost" size="md">
