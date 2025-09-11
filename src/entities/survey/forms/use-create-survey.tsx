ed34be040071e85dd168bfc05a57274ef035ba85
Rafinur Mursalimov <ArtArtistRM@yandex.ru>
2025-09-12 01:01:12 +0300

    Revert "fix: correct rules for validation"
    This reverts commit 7d362e76a4762e3f31e63984610e262239f07bdc.

--- src/entities/survey/forms/use-create-survey.tsx
+++ src/entities/survey/forms/use-create-survey.tsx
@@ -1,14 +1,9 @@
 import { useForm } from '@mantine/form'
 
-// TODO: Поменять типы группа/участник на реальные
-type TGroup = 'groupTest'
-type TMember = 'memberTest'
-type ParticipantsValue = (TGroup | TMember)[]
-
 interface IInitialValues {
-  participants: ParticipantsValue | []
-  beginDate: Date | null
-  endDate: Date | null
+  participants: string[] // TODO: Вставить типы сущностей группа/участник
+  beginDate: Date
+  endDate: Date
   commentary?: string
 }
 
@@ -16,45 +11,32 @@ export const useCreateSurvey = (initialValues: IInitialValues) => {
   const formSurveyData = useForm({
     mode: 'uncontrolled',
     initialValues,
-    validateInputOnChange: true,
     validate: {
       participants: value => {
-        if (value?.length < 1) return 'Необходимо выбрать участников или группу'
-        if (value.length === 1 && value[0] === 'memberTest') {
-          return 'Выберите больше одного участника опроса либо группу'
-        }
+        if (!value) return null
+        return value.length < 1 ? 'Должно быть минимум 2 участника или группа' : null
       },
-      beginDate: (value, values) => {
-        if (!value) return 'Выберите дату начала опроса'
+      beginDate: value => {
+        if (!value) return null
         const selectedDate = new Date(value)
         const today = new Date()
         today.setHours(0, 0, 0, 0)
         selectedDate.setHours(0, 0, 0, 0)
         if (selectedDate < today) {
-          return 'Нельзя выбрать дату начала опроса в прошлом.'
-        }
-        if (values.endDate && new Date(values.endDate) < selectedDate) {
-          return 'Дата начала не может быть позже завершения опроса.'
+          return 'Нельзя выбрать дату в прошлом.'
         }
         return null
       },
-      endDate: (value, values) => {
-        if (!value) return 'Выберите дату завершения опроса'
-        if (values.beginDate) {
-          const selectedDate = new Date(value)
-          const begin = new Date(values.beginDate)
-          selectedDate.setHours(0, 0, 0, 0)
+      endDate: value => {
+        if (!value) return null
+        const beginValue = formSurveyData.getValues().beginDate as Date | null
+        if (beginValue) {
+          const end = new Date(value)
+          const begin = new Date(beginValue)
+          end.setHours(0, 0, 0, 0)
           begin.setHours(0, 0, 0, 0)
-          if (selectedDate < begin) {
-            return 'Дата завершения должна быть позже даты начала.'
-          }
-        }
-        if (!values.beginDate) {
-          const today = new Date()
-          today.setHours(0, 0, 0, 0)
-          const selectedDate = new Date(value)
-          if (selectedDate < today) {
-            return 'Нельзя выбрать дату завершения опроса в прошлом.'
+          if (end < begin) {
+            return 'Дата завершения должна быть не раньше даты начала.'
           }
         }
         return null
@@ -68,33 +50,3 @@ export const useCreateSurvey = (initialValues: IInitialValues) => {
 
   return formSurveyData
 }
-
-// NOTES
-// This is a test stand
-// <DatePickerInput /> required mantine 8.3.0!
-//
-//
-// <form onSubmit={formSurveyData.onSubmit(console.log)}>
-//   <MultiSelect
-//     label="Кто участвует"
-//     key={formSurveyData.key('participants')}
-//     clearable
-//     nothingFoundMessage="Ничего не найдено"
-//     {...formSurveyData.getInputProps('participants')}
-//   />
-//   <DatePickerInput
-//     label="Начало"
-//     clearable
-//     // value={value}
-//     // onChange={setValue}
-//     {...formSurveyData.getInputProps('beginDate')}
-//   />
-//   <DatePickerInput
-//     label="Завершение"
-//     clearable
-//     //   value={value}
-//     // onChange={setValue}
-//     {...formSurveyData.getInputProps('endDate')}
-//   />
-//   <TextInput radius="xs" label="Комментарий" {...formSurveyData.getInputProps('commentary')} />
-// </form>
