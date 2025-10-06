a7c9281f8cf88c3f29572f9fce15e9d1df09d1b9
Diliara <novoobrashennaya@yandex.ru>
2025-10-06 13:54:38 +0300

    fix: данные для Menu с бэкенда, навигация по клику MenuLink, стили

--- src/widgets/menu/types/menu-type.ts
+++
@@ -1,5 +0,0 @@
-import { TMenuLink } from '@/shared/ui/menu-link/types/menu-link-type'
-
-export type TMenuProps = {
-  linksInfo: TMenuLink[]
-}
--- src/widgets/menu/ui/menu.tsx
+++ src/widgets/menu/ui/menu.tsx
@@ -1,11 +1,45 @@
 import { Flex } from '@mantine/core'
 import { MenuLink } from '@/shared/ui/menu-link'
-import { TMenuProps } from '../types/menu-type'
+import { TMenuLink } from '@/shared/ui/menu-link/types/menu-link-type'
+import { routes } from '@/shared/configs/routs'
+import { useDepartmentQuery } from '@/entities/groups'
 
-export const Menu: React.FC<TMenuProps> = ({ linksInfo }: TMenuProps) => {
+export const Menu: React.FC = () => {
+  const { data } = useDepartmentQuery()
+  const departmentLinks = data
+    ? data.map(department => ({
+        id: department.id.toString(),
+        name: department.department_name,
+        queryKey: 'department',
+        queryValue: department.id.toString()
+      }))
+    : []
+  const links: TMenuLink[] = [
+    {
+      id: 'Опросы',
+      link: routes.surveys(),
+      name: 'Опросы',
+      children: departmentLinks
+    },
+    {
+      id: 'Люди',
+      link: routes.employees(),
+      name: 'Люди'
+    },
+    {
+      id: 'Команды',
+      link: routes.departments(),
+      name: 'Команды'
+    },
+    {
+      id: 'Вопросы',
+      link: routes.questions(),
+      name: 'Вопросы'
+    }
+  ]
   return (
     <Flex direction="column" gap="4px" component="nav">
-      {linksInfo.map(linkInfo => {
+      {links.map(linkInfo => {
         return <MenuLink key={linkInfo.id} {...linkInfo} />
       })}
     </Flex>
