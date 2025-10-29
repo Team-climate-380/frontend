b1605e24b3338fe1b1a82a4c288e65eff59739ce
Marina Sysoliatina <stereogam@yandex.ru>
2025-10-29 13:53:15 +0100

    fix/fixed question list loader

--- src/pages/questions/ui/questions.tsx
+++ src/pages/questions/ui/questions.tsx
@@ -8,7 +8,8 @@ import { useState, useEffect } from 'react'
 import { useQueryParams } from '@/shared/hooks/useQueryParams'
 import { getQuestions } from '@/entities/question/api/get-questions'
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { Loader } from '@shared/ui/loader'
+import { Loader } from '@/shared/ui/loader'
+// import { Skeleton } from '@/shared/ui/skeleton'
 import { useIntersection } from '@mantine/hooks'
 import { QuestionForm } from '@/features/question-form'
 import { QuestionsList } from '@/features/questions-list'
@@ -120,7 +121,6 @@ const QuestionPage = () => {
             />
           </div>
         )}
-        {/* TODO implement form from #52 task}*/}
         <div className={styles['questions-list']}>
           <QuestionsList
             questions={data?.pages.flatMap(pageItem => pageItem?.data).filter((q): q is IQuestion => Boolean(q)) ?? []}
@@ -128,7 +128,7 @@ const QuestionPage = () => {
         </div>
       </div>
       <div ref={ref} className={styles.loader_container}>
-        {isFetchingNextPage && <Loader />}
+        {isFetchingNextPage && <Loader size="lg" />}
       </div>
     </div>
   )
